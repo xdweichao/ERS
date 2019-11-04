23e314a08d4210e29190158a43f4c337bc05a972
Wei Liang <43675973+xdweichao@users.noreply.github.com>
2019-11-04 10:37:41 -0500

    Basic Login [Not Connected to DB yet]

---
+++ src/main/java/com/revature/util/ConnectionUtil.java
@@ -0,0 +1,20 @@
+package com.revature.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+
+	public static Connection getConnection() {
+		String url = "jdbc:postgresql://ersdatabase.cb4kahoa7axw.us-east-2.rds.amazonaws.com:5432/postgres";
+		try {
+			return DriverManager.getConnection(url, "postgres",  "revature");
+				//System.getenv("Wei_Username"),System.getenv("Wei_Password"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Unable to connect to database.");
+			return null;
+		}
+	}
+}
